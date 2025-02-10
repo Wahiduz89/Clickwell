45155c4132fdd3596823923c04edc36b4a74b2e6
Wahiduz Zaman <156948092+Wahiduz89@users.noreply.github.com>
2025-02-10 18:22:56 +0530

    Update ProductItem.jsx

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -7,13 +7,25 @@ const ProductItem = ({id,image,name,price}) => {
     const {currency} = useContext(ShopContext);
 
   return (
-    <Link onClick={()=>scrollTo(0,0)} className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
-      <div className=' overflow-hidden'>
-        <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt="" />
-      </div>
-      <p className='pt-3 pb-1 text-sm'>{name}</p>
-      <p className=' text-sm font-medium'>{currency}{price}</p>
-    </Link>
+    <Link
+    onClick={() => scrollTo(0, 0)}
+    className="text-gray-700 cursor-pointer"
+    to={`/product/${id}`}
+  >
+    <div className="overflow-hidden rounded-lg transform hover:scale-105 hover:border-blue-500 transition-all duration-300">
+      <img
+        className="hover:scale-110 transition ease-in-out"
+        src={image[0]}
+        alt=""
+      />
+    </div>
+    <p className="pt-3 pb-1 text-sm">{name}</p>
+    <p className="text-sm font-medium">
+      {currency}
+      {price}
+    </p>
+  </Link>
+  
   )
 }
 
